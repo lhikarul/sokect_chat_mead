d721ec718a5f451858c912c94358e964b5c797ea
lhiakrul <711hikaru@gmail.com>
2020-08-30 11:24:13 +0800

    add: rendering location messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,11 +10,20 @@ const $messages = document.querySelector("#messages")
 // Template
 const messageTemplate = document.querySelector("#message-template").innerHTML
 
+const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML
+
 socket.on('message',(message) => {
     const html = Mustache.render(messageTemplate, {
         message
     });
     $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (url) => {
+    const html = Mustache.render(locationMessageTemplate, {
+        url
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
